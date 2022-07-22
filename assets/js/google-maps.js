e70536c852e6ec1dd080e72dc3778416a0b34aa2
Alejandro Sánchez Monzón <90842732+AlejandroSanchezMonzon@users.noreply.github.com>
2022-07-22 13:33:51 +0200

    Update google-maps.js

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 40.4165, lng: -3.70256};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Madrid, España' // Title Location
     });
 }
